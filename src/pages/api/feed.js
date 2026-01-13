d5f0c53b985312da7c7be8ce9218e41c3a32fbdb
reshuk-code <reshuksapkota2007@gmail.com>
2026-01-13 14:18:32 +0545

    explor page request error fixed

--- src/pages/api/feed.js
+++ src/pages/api/feed.js
@@ -78,9 +78,15 @@ export default async function handler(req, res) {
                         }
                     },
                     interestScore: {
-                        $switch: {
-                            branches: interestBranches,
-                            default: 0
+                        $let: {
+                            vars: { branches: interestBranches },
+                            in: {
+                                $cond: {
+                                    if: { $gt: [{ $size: '$$branches' }, 0] },
+                                    then: { $switch: { branches: '$$branches', default: 0 } },
+                                    else: 0
+                                }
+                            }
                         }
                     }
                 }
